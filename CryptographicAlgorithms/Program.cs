be9e5c19dce4247363b20af9c5fd7e8e39000f43
Alexander <informery4@gmail.com>
2021-06-06 17:23:55 +0300

    Добавьте файлы проекта.

---
+++ CryptographicAlgorithms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CryptographicAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
